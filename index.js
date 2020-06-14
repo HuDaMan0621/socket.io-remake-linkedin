4593427c506f5110e179d43cbf25cbe869b30f5d
hudaman0621 <mrhu828@gmail.com>
2020-06-14 14:50:58 -0400

    3 chat room socket io

--- index.js
+++ index.js
@@ -3,20 +3,41 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
-server.listen(port, ()=> {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+})
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html');
+})
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html');
+})
 
-io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.emit('message', { testuser: 'hey how are you?'});
-    socket.on('another event', (data) => {
-        console.log(data);
+//tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New User Joined ${data.room} room!`); //emiting a message to the room
+    })
+    // console.log('user connected'); //server emits User connected
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+
+        tech.emit('message', 'User Disconnected'); //server emits everyone someone disconnected
     })
 })
